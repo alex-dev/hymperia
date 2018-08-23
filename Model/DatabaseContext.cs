43d592f3d72f66ef4d18f5378de4384bee996c1f
Alexandre Parent <alexandreparent_dev@outlook.com>
2018-08-23 12:25:55 -0400

    Initial dump database ORM context

---
+++ Model/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace InfoApp.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        const string ConfigurationName = "MainDatabase";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(ConfigurationManager.ConnectionStrings[ConfigurationName].ConnectionString);
+        }
+    }
+}
