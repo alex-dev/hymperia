9b64d4a27c4b8e206d25a0a9dfe96f64e12e190b
Guillaume LE BLANC <1457093@cstj.qc.ca>
2018-09-18 16:31:57 -0400

    Manipulator Exploration

---
+++ HelixViewport3DTest/ManipulatorExploration.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hymperia.HelixViewport3DTest
+{
+  // Ref: https://github.com/helix-toolkit/helix-toolkit/tree/develop/Source/Examples/WPF/ExampleBrowser/Examples/Manipulator
+  public partial class ManipulatorExploration : UserControl
+    {
+        public ManipulatorExploration()
+        {
+            InitializeComponent();
+        }
+    }
+}
