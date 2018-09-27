c23c5f94e906d3f34d7b97e30cac8c395f42b228
Alexandre PARENT <1237801@cstj.qc.ca>
2018-09-27 14:56:45 -0400

    Tests updates

---
+++ ModelIntegrationTests/MockedContext.cs
@@ -0,0 +1,13 @@
+﻿using Hymperia.Model;
+
+namespace ModelIntegrationTests
+{
+  public class MockedContext : DatabaseContext
+  {
+    public string DatabaseString => Connection;
+
+    public MockedContext() : base() { }
+
+    public MockedContext(string database) : base(database) { }
+  }
+}
---
+++ ModelIntegrationTests/MockedContextIsWorking.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModelIntegrationTests
+{
+  [TestClass]
+  public class MockedContextIsWorking
+  {
+    private string DatabaseString { get; set; }
+
+    [TestInitialize]
+    public async Task Initialize()
+    {
+      using (var context = new MockedContext())
+      {
+        DatabaseString = context.DatabaseString;
+        await context.Migrate();
+      }
+    }
+
+    [TestCleanup]
+    public async Task Cleanup()
+    {
+      using (var context = new MockedContext(DatabaseString))
+      {
+        await context.Database.EnsureDeletedAsync();
+      }
+    }
+
+    [TestMethod]
+    public async Task ShouldGenerateTemporaryContext()
+    {
+      using (var context = new MockedContext(DatabaseString))
+      {
+        try
+        {
+          await context.Projets.CountAsync();
+        }
+        catch (Exception)
+        {
+          Assert.Fail();
+        }
+      }
+    }
+  }
+}
