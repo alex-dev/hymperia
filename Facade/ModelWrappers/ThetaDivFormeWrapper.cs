8836442f5cc55b1a6ce4fa57cebd2d80c519f770
Antoine MAILHOT <1339508@cstj.qc.ca>
2018-09-27 18:01:12 -0400

    Avancement T6.3

--- Facade/ModelWrappers/ThetaDivFormeWrapper.cs
+++ Facade/ModelWrappers/ThetaDivFormeWrapper.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 using Hymperia.Model.Modeles;
+using Hymperia.Model.Modeles.JsonObject;
+
 
 namespace Hymperia.Facade.ModelWrappers
 {
-  /*public class ThetaDivFormeWrapper<T> : FormeWrapper<T> where T : ThetaDivForme
+  public class ThetaDivFormeWrapper<T> : FormeWrapper<T> where T : ThetaDivForme
   {
-  }*/
+    #region Attributs
 
-  /*public class ThetaDivFormeWrapper : FormeWrapper<T>
-  {
-  }*/
+    public int ThetaDiv
+    {
+      get => Forme.ThetaDiv;
+      set
+      {
+        Forme.ThetaDiv = value;
+        OnPropertyChanged();
+      }
+    }
+    #endregion
+
+    public ThetaDivFormeWrapper(T forme) : base(forme) { }
+  }
 }
