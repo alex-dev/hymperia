60c2eb094f5e02f820686d1f5d77d8f463f66ea0
Alexandre PARENT <1237801@cstj.qc.ca>
2018-09-28 17:42:12 -0400

    DirectxResearch

--- Facade/GeometryExtension.cs
+++ Facade/GeometryExtension.cs
@@ -1,23 +1,17 @@
-﻿using System.Windows.Media.Media3D;
-using Hymperia.Model.Modeles.JsonObject;
+﻿using Media = System.Windows.Media.Media3D;
+using Object = Hymperia.Model.Modeles.JsonObject;
 
 namespace Hymperia.Facade
 {
   public static class GeometryExtension
   {
-    public static Point Convert(this Point3D point) => new Point(point.X, point.Y, point.Z);
-    public static Point3D Convert(this Point point) => new Point3D(point.X, point.Y, point.Z);
+    public static Object.Point Convert(this Media.Point3D point) => new Object.Point(point.X, point.Y, point.Z);
+    public static Media.Point3D Convert(this Object.Point point) => new Media.Point3D(point.X, point.Y, point.Z);
 
-    public static Vector Convert(this Vector3D vector) => new Vector(vector.X, vector.Y, vector.Z);
-    public static Vector3D Convert(this Vector vector) => new Vector3D(vector.X, vector.Y, vector.Z);
+    public static Object.Vector Convert(this Media.Vector3D vector) => new Object.Vector(vector.X, vector.Y, vector.Z);
+    public static Media.Vector3D Convert(this Object.Vector vector) => new Media.Vector3D(vector.X, vector.Y, vector.Z);
 
-    public static Model.Modeles.JsonObject.Quaternion Convert(this System.Windows.Media.Media3D.Quaternion quaternion)
-    {
-      return new Model.Modeles.JsonObject.Quaternion(quaternion.X, quaternion.Y, quaternion.Z, quaternion.W);
-    }
-    public static System.Windows.Media.Media3D.Quaternion Convert(this Model.Modeles.JsonObject.Quaternion quaternion)
-    {
-      return new System.Windows.Media.Media3D.Quaternion(quaternion.X, quaternion.Y, quaternion.Z, quaternion.W);
-    }
+    public static Object.Quaternion Convert(this Media.Quaternion quaternion) => new Object.Quaternion(quaternion.X, quaternion.Y, quaternion.Z, quaternion.W);
+    public static Media.Quaternion Convert(this Object.Quaternion quaternion) => new Media.Quaternion(quaternion.X, quaternion.Y, quaternion.Z, quaternion.W);
   }
 }
