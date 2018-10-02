0bc5a6c42890773381041608ec5f3b95d19e27fe
Guillaume LE BLANC <1457093@cstj.qc.ca>
2018-10-02 16:10:29 -0400

    Updated

---
+++ Facade/ViewModels/RegionContextBase.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Mvvm;
+
+namespace Hymperia.Facade.ViewModels
+{
+  public abstract class RegionContextBase : BindableBase
+  {
+    private object context;
+
+    public object RegionContext
+    {
+      get => context;
+      set => SetProperty(ref context, value, OnRegionContextChanged);
+    }
+
+    protected virtual void OnRegionContextChanged() { }
+  }
+}
