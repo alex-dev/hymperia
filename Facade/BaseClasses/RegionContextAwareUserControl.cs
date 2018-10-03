1a7b6853598ae5cad4da56ef3ea81fa8f8188ed9
Alexandre Parent <alex-dev@users.noreply.github.com>
2018-10-03 10:50:38 -0400

    Update RegionContextAwareUserControl.cs

--- Facade/BaseClasses/RegionContextAwareUserControl.cs
+++ Facade/BaseClasses/RegionContextAwareUserControl.cs
@@ -23,12 +23,17 @@ namespace Hymperia.Facade.BaseClasses
 
     protected RegionContextAwareUserControl()
     {
-      Load += BindRegionContext;
+      Load += BindRegionContext; // Attends jusqu'à ce que tous les bindings de RegionContext soient résolus pour connecter le RegionContext
+                                 // de la vue parente au RegionContext de la vue enfant.
     }
     
     private void BindRegionContext()
     {
-      BindingOperations.SetBinding(R.RegionContext.GetObservableContext(this), ObservableObject<object>.ValueProperty, new Binding("RegionContext") { Source = this, Mode = BindingMode.OneWayToSource });
+      if (BindingOperations.GetBinding(R.RegionContext.GetObservableContext(this), ObservableObject<object>.ValueProperty) is null)
+      {
+        BindingOperations.SetBinding(R.RegionContext.GetObservableContext(this), ObservableObject<object>.ValueProperty, new Binding("RegionContext") { Source = this, Mode = BindingMode.OneWayToSource });
+      }
+      
       /*R.RegionContext.GetObservableContext(this).PropertyChanged += (sender, args) =>
       {
         RegionContext = ((ObservableObject<object>)sender).Value;
