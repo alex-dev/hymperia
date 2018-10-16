79af6b7af43e175549a22a21bad6019bb9b4eec3
Alexandre Parent <alexandreparent_dev@outlook.com>
2018-10-16 04:56:29 -0400

    Fixed assemblynames

--- Hymperia/Properties/AssemblyInfo.cs
+++ Hymperia/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Facade")]
+[assembly: AssemblyTitle("Hymperia")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Facade")]
+[assembly: AssemblyProduct("Hymperia")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
