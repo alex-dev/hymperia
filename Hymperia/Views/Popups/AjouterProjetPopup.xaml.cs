beccd1f01c935bb18ec36bc9c644e5794c0d492a
Antoine Mailhot <antoine1995@live.ca>
2018-10-31 02:18:07 -0400

    Avancement T19.2

---
+++ Hymperia/Views/Popups/AjouterProjetPopup.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Prism.Interactivity.InteractionRequest;
+
+namespace Hymperia.Facade.Views.Popups
+{
+  /// <summary>
+  /// Logique d'interaction pour AjouterProjet.xaml
+  /// </summary>
+  public partial class AjouterProjetPopup : UserControl, IInteractionRequestAware
+  {
+    public Action FinishInteraction { get; set; }
+    public INotification Notification { get; set; }
+
+    public AjouterProjetPopup()
+    {
+      InitializeComponent();
+    }
+
+    private void Confirm(object sender, RoutedEventArgs e)
+    {
+      if (Notification is IConfirmation confirmation)
+      {
+        confirmation.Confirmed = true;
+      }
+
+      FinishInteraction?.Invoke();
+    }
+
+    private void Cancel(object sender, RoutedEventArgs e)
+    {
+      if (Notification is IConfirmation confirmation)
+      {
+        confirmation.Confirmed = false;
+      }
+
+      FinishInteraction?.Invoke();
+    }
+
+  }
+}
