5741584e8db1196b5a413f3b2512fc0afb4ceb92
Antoine MAILHOT <1339508@cstj.qc.ca>
2018-11-01 18:32:27 -0400

    Avancement T19.2

---
+++ Hymperia/ValidationRules/StringNotNullOrWhiteSpaceRule.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using Hymperia.Facade.Properties;
+
+namespace Hymperia.Facade.ValidationRules
+{
+  public class StringNotNullOrWhiteSpaceRule : ValidationRule
+  {
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo) =>
+      string.IsNullOrWhiteSpace(value as string)
+        ? new ValidationResult(false, Resources.FieldCannotBeEmpty)
+        : ValidationResult.ValidResult;
+  }
+}
