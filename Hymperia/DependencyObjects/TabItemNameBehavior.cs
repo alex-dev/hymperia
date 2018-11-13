147afe185244dfc1185c0a102374cfdda363332c
Guillaume LE BLANC <1457093@cstj.qc.ca>
2018-11-13 17:54:31 -0500

    TabItemNameBehavior

---
+++ Hymperia/DependencyObjects/TabItemNameBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace Hymperia.Facade.DependencyObjects
+{
+  public static class TabItemNameBehavior
+  {
+    private const string TabItemName = nameof(TabItemName);
+
+    public static readonly DependencyProperty TabItemNameProperty =
+      DependencyProperty.RegisterAttached(
+        TabItemName,
+        typeof(string),
+        typeof(TabItemNameBehavior),
+        new PropertyMetadata(string.Empty));
+
+    public static void SetSourceResourceKey(DependencyObject element, string value) =>
+      element.SetValue(TabItemNameProperty, value);
+
+    public static string GetSourceResourceKey(DependencyObject element) =>
+      (string)element.GetValue(TabItemNameProperty);
+
+  }
+}
