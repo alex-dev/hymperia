36c8839eeaa13440263e969374e4129238cc2615
Alexandre PARENT <1237801@cstj.qc.ca>
2018-11-20 18:23:33 -0500

    gdf

--- Hymperia/DependencyObjects/NumericTextBoxBehavior.cs
+++ Hymperia/DependencyObjects/NumericTextBoxBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Prism.Commands;
 
 namespace Hymperia.Facade.DependencyObjects
 {
@@ -12,36 +13,56 @@ namespace Hymperia.Facade.DependencyObjects
     private const string DownCommand = nameof(DownCommand);
     private const string DownCommandParameter = nameof(DownCommandParameter);
 
-    public static readonly DependencyProperty UpCommandProperty =
-      DependencyProperty.RegisterAttached(UpCommand, typeof(ICommand),  typeof(NumericTextBoxBehavior));
+    private static readonly ICommand Up = new DelegateCommand<TextBox>(ExecuteUp);
+    private static readonly ICommand Down = new DelegateCommand<TextBox>(ExecuteDown);
 
-    public static readonly DependencyProperty UpCommandParameterProperty =
-      DependencyProperty.RegisterAttached(UpCommandParameter, typeof(object), typeof(NumericTextBoxBehavior));
+    public static readonly DependencyProperty UpCommandProperty =
+      DependencyProperty.RegisterAttachedReadOnly(UpCommand, typeof(ICommand),  typeof(NumericTextBoxBehavior), new PropertyMetadata(Up))
+      .DependencyProperty;
 
     public static readonly DependencyProperty DownCommandProperty =
-      DependencyProperty.RegisterAttached(DownCommand, typeof(ICommand), typeof(NumericTextBoxBehavior));
+      DependencyProperty.RegisterAttachedReadOnly(DownCommand, typeof(ICommand), typeof(NumericTextBoxBehavior), new PropertyMetadata(Down))
+      .DependencyProperty;
+
+    public static readonly DependencyProperty UpCommandParameterProperty =
+      DependencyProperty.RegisterAttached(UpCommandParameter, typeof(double), typeof(NumericTextBoxBehavior), new PropertyMetadata(1.0));
 
     public static readonly DependencyProperty DownCommandParameterProperty =
-      DependencyProperty.RegisterAttached(DownCommandParameter, typeof(object), typeof(NumericTextBoxBehavior));
+      DependencyProperty.RegisterAttached(DownCommandParameter, typeof(double), typeof(NumericTextBoxBehavior), new PropertyMetadata(1.0));
 
-    public static object GetUpCommand(TextBox element) =>
-      element.GetValue(UpCommandProperty);
-    public static void SetUpCommand(TextBox element, object value) =>
-      element.SetValue(UpCommandProperty, value);
+    public static ICommand GetUpCommand(TextBox element) =>
+      element.GetValue(UpCommandProperty) as ICommand;
+    public static ICommand GetDownCommand(TextBox element) =>
+      element.GetValue(DownCommandProperty) as ICommand;
 
-    public static object GetUpCommandParameter(TextBox element) =>
-      element.GetValue(UpCommandParameterProperty);
+    public static double GetUpCommandParameter(TextBox element) =>
+      element.GetValue(UpCommandParameterProperty) as double? ?? 0;
     public static void SetUpCommandParameter(TextBox element, object value) =>
       element.SetValue(UpCommandParameterProperty, value);
 
-    public static object GetDownCommand(TextBox element) =>
-      element.GetValue(DownCommandProperty);
-    public static void SetDownCommand(TextBox element, object value) =>
-      element.SetValue(DownCommandProperty, value);
-
-    public static object GetDownCommandParameter(TextBox element) =>
-      element.GetValue(DownCommandParameterProperty);
+    public static double GetDownCommandParameter(TextBox element) =>
+      element.GetValue(DownCommandParameterProperty) as double? ?? 0;
     public static void SetDownCommandParameter(TextBox element, object value) =>
       element.SetValue(DownCommandParameterProperty, value);
+
+    private static void ExecuteUp(TextBox target)
+    {
+      double increment = GetUpCommandParameter(target);
+
+      if (!double.TryParse(target.Text, out double value))
+        value = 0;
+
+      target.Text = (value + increment).ToString();
+    }
+
+    private static void ExecuteDown(TextBox target)
+    {
+      double increment = GetUpCommandParameter(target);
+
+      if (!double.TryParse(target.Text, out double value))
+        value = 0;
+
+      target.Text = (value - increment).ToString();
+    }
   }
 }
