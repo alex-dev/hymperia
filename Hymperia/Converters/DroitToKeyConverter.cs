2105132081e7cf93d8f176ddeb14243711e48bf6
Guillaume LE BLANC <1457093@cstj.qc.ca>
2018-12-06 14:23:23 -0500

    Yes

---
+++ Hymperia/Converters/DroitToKeyConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Hymperia.Facade.Extensions;
+using Hymperia.Model.Modeles;
+
+namespace Hymperia.Facade.Converters
+{
+  [ValueConversion(typeof(Acces.Droit), typeof(string))]
+  public sealed class DroitToKeyConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+      (value as Acces.Droit? ?? Acces.Droit.Lecture).ToString();
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      EnumExtension.Parse<Acces.Droit>(value?.ToString());
+  }
+}
---
+++ Hymperia/Extensions/EnumExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hymperia.Facade.Extensions
+{
+  public static class EnumExtension
+  {
+    public static TEnum Parse<TEnum>(string value) where TEnum : struct
+    {
+      if (!Enum.TryParse(value, out TEnum result))
+        throw new InvalidOperationException();
+
+      return result;
+    }
+  }
+}
